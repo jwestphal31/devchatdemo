789d3343c1aed50aad097b14694971ab6fd83b3f
Jeff Thorne <jeff.thorne@gmail.com>
2018-11-06 19:29:01 +0000

    first commit

---
+++ app/app.py
@@ -0,0 +1,29 @@
+import os
+from flask import Flask, render_template, request, flash, redirect
+from flask_assets import Environment
+from webassets import loaders
+import boto3
+
+
+
+app = Flask(__name__, static_folder='static', static_url_path='/static')
+app.config['DEBUG'] = False
+app.config['TEMPLATES_AUTO_RELOAD'] = False
+app.config['SECRET_KEY'] = "123"
+
+# asset pipeline
+assets = Environment(app)
+#bundles = loaders.YAMLLoader('./static/js/js-assets.yml').load_bundles()
+#[assets.register(name, bundle) for name, bundle in bundles.items()]
+##bundles = loaders.YAMLLoader('./static/styles/css-assets.yml').load_bundles()
+#[assets.register(name, bundle) for name, bundle in bundles.items()]
+
+
+@app.route("/")
+def index():
+    return "hello world"
+
+
+
+if __name__ == "__main__":
+    app.run(debug=False,host='0.0.0.0', port=80)
